e8e97c41f36d748697deadbb5f3e641ed523cf34
Inna018 <122346373+Inna018@users.noreply.github.com>
2024-02-13 08:56:48 +0200

    add new file

---
+++ bundle.js
@@ -0,0 +1,21 @@
+(function (factory) {
+  typeof define === "function" && define.amd ? define(factory) : factory();
+})(function () {
+  "use strict";
+
+  function add(f, s) {
+    return f + s;
+  }
+
+  function sub(f, s) {
+    return f - s;
+  }
+
+  const a$1 = 1;
+  console.lof(a$1);
+
+  const a = 2;
+  console.log(sub(1, 3));
+  console.log(add(1, 3));
+  console.log(a);
+});
---
+++ calc.js
@@ -0,0 +1,14 @@
+export function add(f, s) {
+  return f + s;
+}
+
+export function sub(f, s) {
+  return f - s;
+}
+
+export function div(f, s) {
+  return f / s;
+}
+
+const a = 1;
+console.lof(a);
